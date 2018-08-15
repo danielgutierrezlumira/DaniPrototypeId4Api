a1cd19ae0afe9f849cc1872834902a7f1d9a9d9e
Daniel Gutiérrez <daniel.gutierrez@inrstar.com>
2018-08-15 09:12:39 +0100

    Expanded values copntroller

--- ExampleApi/Controllers/ValuesController.cs
+++ ExampleApi/Controllers/ValuesController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExampleApi.Controllers
@@ -10,36 +8,70 @@ namespace ExampleApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private static readonly IDictionary<int, string> Values = new Dictionary<int, string>
+        {
+            {1, @"value1"},
+            {2, @"value2"}
+        };
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IDictionary<int, string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(Values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            if (!Values.ContainsKey(id))
+            {
+                return NotFound();
+            }
+
+            return Ok(Values[id]);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] string value)
         {
+            var counter = 0;
+
+            while (!Values.TryAdd(GetHashCode(), value) || counter++ < 100)
+            {
+            }
+
+            return counter < 100 ? Ok() : throw new TimeoutException();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, [FromBody] string value)
         {
+            if (!Values.ContainsKey(id))
+            {
+                return NotFound();
+            }
+
+            Values[id] = value;
+
+            return Ok();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            if (!Values.ContainsKey(id))
+            {
+                return NotFound();
+            }
+
+            Values.Remove(id);
+
+            return Ok();
         }
     }
 }
